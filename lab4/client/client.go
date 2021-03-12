9cd31ac3176ee0fe10aa7014cda7acc7abf0d9b9
oyssund <oyssund@gmail.com>
2021-03-12 14:59:00 +0100

    merge all in lab4

--- lab4/client/client.go
+++ lab4/client/client.go
@@ -16,6 +16,144 @@ import (
 	"time"
 )
 
+func main() {
+	fmt.Println("Hello R2-D2, your secret ID for this mission is:")
+
+	thisClient := createRandomID()
+	ip, port := GetOutboundIP()
+
+	thisaddress := ip.String() + ":" + strconv.Itoa(port)
+	tcpadd, err := net.ResolveTCPAddr("tcp", thisaddress)
+
+	TCPListn, err := net.ListenTCP("tcp", tcpadd)
+
+	fmt.Println(thisClient, " and listen", TCPListn)
+	// get configuration file
+	netconfigureFile, err := os.Open("../app/netconf.json")
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	defer netconfigureFile.Close()
+
+	//1.2 Read the network config file as byte array
+	byteVal, _ := ioutil.ReadAll(netconfigureFile)
+
+	//initialize a netconfig struct to save the netconfig
+	var netconf network.Netconf
+	err = json.Unmarshal(byteVal, &netconf)
+	if err != nil {
+		log.Print(err)
+	}
+
+	//1.3 Now netconf has the info from json file, step 1 complete
+
+	// send channel value
+	CSend := make(chan mp.Value, 2000000)
+
+	// recieve channel network message
+	recieveEntireMessage := make(chan network.Message, 2000000)
+
+	// we need a map to store connection info
+	//Setting up connections
+	connections := make(map[int]net.Conn)
+	connections = dialUp(netconf, connections, thisClient, recieveEntireMessage)
+	delay := 3 * time.Second
+	// sequence number
+	seq := 0
+	responseRecieved := true
+
+	go func() {
+		for {
+			reader := bufio.NewReader(os.Stdin)
+
+			fmt.Print("--> ")
+			text, _ := reader.ReadString('\n')
+
+			text = text[:len(text)-1]
+			if text == "retry connections" {
+				connections = dialUp(netconf, connections, thisClient, recieveEntireMessage)
+				continue
+			}
+			if text == "show seq" {
+				fmt.Println("Current seq", seq)
+				continue
+			}
+			if text == "reset seq" {
+				seq = 0
+				fmt.Println("Current seq", seq)
+				responseRecieved = true
+				continue
+			}
+			if text == "show connections" {
+				fmt.Println(connections)
+			} else {
+				if responseRecieved {
+					// NEED TO SEND data to servers
+					newVal := mp.Value{ClientID: thisClient, ClientSeq: seq,
+						Command: text}
+					CSend <- newVal
+
+					responseRecieved = false
+				} else {
+					fmt.Println("Command can not be entered as no response to previous command has arrived")
+				}
+
+			}
+
+		}
+
+	}()
+
+	go func() {
+		for {
+
+			if len(connections) < 3 {
+
+				//go autoRetry(delay, netconf, connections, thisClient, recieveEntireMessage)
+
+				timer1 := time.NewTimer(delay)
+				<-timer1.C
+				fmt.Println("Not all servers connected, trying to reconnect")
+				connections = dialUp(netconf, connections, thisClient, recieveEntireMessage)
+				if len(connections) < 3 {
+					fmt.Println("did not succeed to connect to all, doubling delay from ", delay, "to", delay*2)
+					delay += delay
+
+				}
+			}
+		}
+	}()
+
+	for {
+
+		select {
+		case sendMsg := <-CSend:
+			sendMessage(sendMsg, connections)
+			// HERE WE SEND TO SERVER
+		case msg := <-recieveEntireMessage:
+			//HERE WE RECIEVE
+			if msg.Type == "Response" {
+				//fmt.Println(msg)
+				if msg.Response.ClientSeq == seq {
+					responseRecieved = true
+					seq++
+				}
+				fmt.Println("melding fra serveren", msg.Response)
+			}
+		}
+
+	}
+}
+
+func autoRetry(delay time.Duration, netconf network.Netconf, connections map[int]net.Conn, thisClient string, recieveEntireMessage chan network.Message) map[int]net.Conn {
+	timer1 := time.NewTimer(delay)
+	<-timer1.C
+	fmt.Println("Not all servers connected, trying to reconnect")
+	connections = dialUp(netconf, connections, thisClient, recieveEntireMessage)
+	return connections
+}
+
 // Get preferred outbound ip of this machine "Copied from online"
 func GetOutboundIP() (net.IP, int) {
 	conn, err := net.Dial("udp", "8.8.8.8:80")
@@ -39,119 +177,7 @@ func createRandomID() string {
 	return strconv.Itoa((ID*rand.Intn(100))%1000 + 2)
 }
 
-func main() {
-	fmt.Println("Hello R2-D2, your secret ID for this mission is:")
-	thisClient := createRandomID()
-	fmt.Println(thisClient)
-	// get configuration file
-	netconfigureFile, err := os.Open("../app/netconf.json")
-	if err != nil {
-		log.Print(err)
-		return
-	}
-	defer netconfigureFile.Close()
-
-	//1.2 Read the network config file as byte array
-	byteVal, _ := ioutil.ReadAll(netconfigureFile)
-
-	//initialize a netconfig struct to save the netconfig
-	var netconf network.Netconf
-	err = json.Unmarshal(byteVal, &netconf)
-	if err != nil {
-		log.Print(err)
-	}
-
-	//1.3 Now netconf has the info from json file, step 1 complete
-	CSend := make(chan mp.Value, 100)
-	Crecieve := make(chan mp.Value, 100)
-	// we need a map to store connection info
-	needInit := false
-
-	connections := make(map[int]net.Conn)
-
-	for _, e := range netconf.Nodes {
-		fmt.Println("Testin", e.ID)
-
-		rAddr, err := net.ResolveTCPAddr("tcp", e.IP+":"+strconv.Itoa(e.Port))
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		fmt.Println("Raddr resolved", rAddr)
-		stringAddr := rAddr.String()
-		d := net.Dialer{Timeout: 3 * time.Second}
-		tcpConn, err := d.Dial("tcp", stringAddr)
-		//tcpConn, err := net.DialTCP("tcp", nil, rAddr)
-
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		// NOE SKJER
-		fmt.Println("connection connected", tcpConn)
-		needInit = true
-		connections[e.ID] = tcpConn
-		go listenForConnection(tcpConn, Crecieve, connections)
-	}
-
-	if len(connections) == 0 {
-		fmt.Println("No connections detected, you are all alone, write \"retry connections\" to try again. or \"show connections\" to show them")
-	} else {
-		fmt.Println(len(connections), " Connections detected")
-	}
-
-	go func() {
-		for {
-			reader := bufio.NewReader(os.Stdin)
-
-			fmt.Print("--> ")
-			text, _ := reader.ReadString('\n')
-
-			text = text[:len(text)-1]
-			if text == "retry connections" {
-				connections = dialUp(netconf, connections, Crecieve, needInit, thisClient)
-				continue
-			}
-			if text == "show connections" {
-				fmt.Println(connections)
-			} else {
-				// NEED TO SEND data to servers
-				newVal := mp.Value{ClientID: thisClient,
-					Command: text}
-				CSend <- newVal
-			}
-
-		}
-
-	}()
-
-	for {
-		if needInit == true {
-			ip, port := GetOutboundIP()
-			str := "INIT|" + ip.String() + ":" + strconv.Itoa(port)
-			init := mp.Value{ClientID: thisClient, Command: str}
-			sendMessage(init, connections)
-			needInit = false
-		}
-		select {
-		case sendMsg := <-CSend:
-			// HERE WE SEND TO SERVER
-			// dummy function
-			sendMessage(sendMsg, connections)
-
-			// fake send:
-
-			//fmt.Println("Sent to server: ", sendMsg)
-		case recieve := <-Crecieve:
-			// here we recieve values.
-			test := recieve.Command + " Executed"
-			fmt.Println(test)
-		}
-
-	}
-}
-
-func dialUp(netconf network.Netconf, connections map[int]net.Conn, Crecieve chan mp.Value, needinit bool, thisClient string) map[int]net.Conn {
+func dialUp(netconf network.Netconf, connections map[int]net.Conn, thisClient string, recieveEntireMessage chan network.Message) map[int]net.Conn {
 
 	for _, e := range netconf.Nodes {
 		fmt.Println("Testin", e.ID)
@@ -175,24 +201,48 @@ func dialUp(netconf network.Netconf, connections map[int]net.Conn, Crecieve chan
 
 		fmt.Println("connection connected", tcpConn)
 		connections[e.ID] = tcpConn
-		go listenForConnection(tcpConn, Crecieve, connections)
+		go listenForConnectiontest(tcpConn, recieveEntireMessage, connections)
 	}
 
 	if len(connections) == 0 {
 		fmt.Println("No connections detected, you are all alone, write \"retry connections\" to try again.")
 	} else {
 		fmt.Println(len(connections), " Connections detected")
-		ip, port := GetOutboundIP()
-		str := "INIT|" + ip.String() + ":" + strconv.Itoa(port)
-		init := mp.Value{ClientID: thisClient, Command: str}
-		sendMessage(init, connections)
+		//ip, port := GetOutboundIP()
+		//str := "INIT|" + ip.String() + ":" + strconv.Itoa(port)
+		//init := mp.Value{ClientID: thisClient, Command: str}
+		//sendMessage(init, connections)
 	}
 	return connections
 }
 
+func listenForConnectiontest(c net.Conn, rchan chan network.Message, connections map[int]net.Conn) (err error) {
+	defer c.Close()
+	buffer := make([]byte, 1024, 1024)
+
+	for {
+		len, err := c.Read(buffer[0:])
+		if err != nil {
+			fmt.Print(err)
+			key := findID(c, connections)
+			fmt.Println("error detected, deleting connection to ", key)
+			if key != -1 {
+				delete(connections, key)
+
+			}
+			return err
+			fmt.Print(err)
+		}
+		msg := new(network.Message)
+		err = json.Unmarshal(buffer[0:len], &msg)
+		if err != nil {
+			fmt.Println(err)
+		}
+		rchan <- *msg
+	}
+
+}
 func sendMessage(sendMsg mp.Value, connections map[int]net.Conn) (err error) {
-	//channel chan<- mp.Value
-	//channel <- sendMsg
 	msg := new(network.Message)
 	msg.Type = "Value"
 	msg.Value = sendMsg
@@ -211,43 +261,6 @@ func sendMessage(sendMsg mp.Value, connections map[int]net.Conn) (err error) {
 	}
 
 	return nil
-}
-
-func listenForConnection(TCPConnection net.Conn, recieveChan chan<- mp.Value, connections map[int]net.Conn) (err error) {
-
-	// defers closing connection until the end
-	defer TCPConnection.Close()
-	//TCPConnection.SetDeadline(time.Now().Add(3*time.Second))
-	buffer := make([]byte, 1024, 1024)
-
-	//etarnal for loop to handle listening to connections
-	for {
-		len, err := TCPConnection.Read(buffer[0:])
-		if err != nil {
-			fmt.Print(err)
-			key := findID(TCPConnection, connections)
-			fmt.Println("error detected, deleting connection to ", key)
-			if key != -1 {
-				delete(connections, key)
-			}
-			return err
-		}
-		message := new(network.Message)
-		err = json.Unmarshal(buffer[0:len], &message)
-		if err != nil {
-			//fmt.Print(err)
-			//key := findID(TCPConnection, connections)
-			//fmt.Println("error detected, deleting connection to ", key)
-			//delete(connections, key)
-			continue
-			return err
-
-		}
-		if message.Type == "Value" {
-			recieveChan <- message.Value
-		}
-
-	}
 }
 
 func findID(tcpConn net.Conn, connections map[int]net.Conn) (key int) {
